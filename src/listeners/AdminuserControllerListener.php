13af6fb7ccacef359e199ab23974571fd7386c03
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-02-12 19:09:46 +0100

    Moved filterForm to Listeners

--- src/listeners/AdminuserControllerListener.php
+++ src/listeners/AdminuserControllerListener.php
@@ -3,7 +3,12 @@
 namespace VitesseCms\User\Listeners;
 
 use Phalcon\Events\Event;
+use VitesseCms\Admin\Forms\AdminlistFormInterface;
+use VitesseCms\Form\Helpers\ElementHelper;
+use VitesseCms\Form\Models\Attributes;
+use VitesseCms\Shop\Controllers\AdmincountryController;
 use VitesseCms\User\Controllers\AdminuserController;
+use VitesseCms\User\Models\PermissionRole;
 use VitesseCms\User\Models\User;
 
 class AdminuserControllerListener
@@ -14,11 +19,33 @@ class AdminuserControllerListener
             && !empty($this->$controller->getPost('new_password'))
             && $controller->user->getPermissionRole() === 'superadmin'
         ) :
-            echo 'hier';
-        die();
             $item->set('password',$controller->security->hash(
                 $controller->request->getPost('new_password'))
             );
         endif;
+    }
+
+    public function adminListFilter(
+        Event $event,
+        AdminuserController $controller,
+        AdminlistFormInterface $form
+    ): string
+    {
+        $form->addNameField($form);
+        $form->addPublishedField($form);
+
+        $form->addText('%CORE_EMAIL%', 'filter[email]')
+            ->addDropdown(
+                'User role',
+                'filter[role]',
+                (new Attributes())->setOptions(
+                    ElementHelper::modelIteratorToOptions($controller->repositories->permissionRole->findAll())
+                )
+            );
+
+        return $form->renderForm(
+            $controller->getLink() . '/' . $controller->router->getActionName(),
+            'adminFilter'
+        );
     }
 }
