edd87a5bfe635e08e09f3dffea52287271fc275a
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-02-13 20:21:46 +0100

    Renamed folders to psr4

--- src/Listeners/InitiateAdminListeners.php
+++ src/Listeners/InitiateAdminListeners.php
@@ -3,6 +3,7 @@
 namespace VitesseCms\User\Listeners;
 
 use Phalcon\Events\Manager;
+use VitesseCms\User\Controllers\AdminpermissionroleController;
 use VitesseCms\User\Controllers\AdminuserController;
 
 class InitiateAdminListeners
@@ -11,5 +12,6 @@ class InitiateAdminListeners
     {
         $eventsManager->attach('adminMenu', new AdminMenuListener());
         $eventsManager->attach(AdminuserController::class, new AdminuserControllerListener());
+        $eventsManager->attach(AdminpermissionroleController::class, new AdminpermissionroleControllerListener());
     }
 }
