97c68a9a518c73f4030baa1074135db559ffc01c
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-05-19 21:22:12 +0200

    Moved BlockUserLogin

---
+++ src/Blocks/UserLogin.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\User\Blocks;
+
+use VitesseCms\Block\AbstractBlockModel;
+use VitesseCms\Block\Models\Block;
+
+class UserLogin extends AbstractBlockModel
+{
+    public function initialize()
+    {
+        parent::initialize();
+
+        $this->excludeFromCache = true;
+    }
+
+    public function parse(Block $block): void
+    {
+        parent::parse($block);
+
+        $block->set('loginUrl', 'user/loginform');
+        $block->set('loggendIn', 0);
+
+        if ($this->di->user->isLoggedIn()):
+            $block->set('loginUrl', 'user/logout');
+            $block->set('loggendIn', 1);
+        endif;
+    }
+}
