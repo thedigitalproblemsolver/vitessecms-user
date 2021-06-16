c134d8f21cef78d548a57fdc6e5655e53a08e8c3
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2021-06-16 19:40:36 +0200

    Added bindByDatagroup to UserFactory.php Added SettingsEnum.php

---
+++ src/Enum/SettingEnum.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace VitesseCms\User\Enums;
+
+use VitesseCms\Core\AbstractEnum;
+
+class SettingEnum extends AbstractEnum
+{
+    public const USER_DATAGROUP_PERSONALINFORMATION = 'USER_DATAGROUP_PERSONALINFORMATION';
+}
