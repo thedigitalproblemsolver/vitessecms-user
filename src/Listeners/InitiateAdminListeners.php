872de2b22dacd7175469f7f9845b6c768a9a981d
Jasper Jaklofsky <info@thedigitalproblemsolver.com>
2023-04-28 11:11:17 -0300

    admin controller changes

--- src/Listeners/InitiateAdminListeners.php
+++ src/Listeners/InitiateAdminListeners.php
@@ -7,12 +7,14 @@ use VitesseCms\Core\Interfaces\InjectableInterface;
 use VitesseCms\User\Controllers\AdminpermissionroleController;
 use VitesseCms\User\Controllers\AdminuserController;
 use VitesseCms\User\Enum\AclEnum;
+use VitesseCms\User\Enum\UserEnum;
 use VitesseCms\User\Enum\UserRoleEnum;
 use VitesseCms\User\Listeners\Admin\AdminMenuListener;
 use VitesseCms\User\Listeners\Admin\AdminMenuPermissionListener;
 use VitesseCms\User\Listeners\Controllers\AdminpermissionroleControllerListener;
 use VitesseCms\User\Listeners\Controllers\AdminuserControllerListener;
 use VitesseCms\User\Listeners\Services\AclServiceListener;
+use VitesseCms\User\Repositories\UserRepository;
 
 class InitiateAdminListeners implements InitiateListenersInterface
 {
@@ -25,5 +27,6 @@ class InitiateAdminListeners implements InitiateListenersInterface
         $di->eventsManager->attach(AdminuserController::class, new AdminuserControllerListener());
         $di->eventsManager->attach(AdminpermissionroleController::class, new AdminpermissionroleControllerListener());
         $di->eventsManager->attach(AclEnum::SERVICE_LISTENER->value, new AclServiceListener($di->acl));
+        $di->eventsManager->attach(UserEnum::SERVICE_LISTENER->value, new UserListener($di->user, new UserRepository()));
     }
 }
